e202fb271ca482e782cfe0ac6825ae40dc39f59e
JenHao <jenhao.hsiao@gmail.com>
2018-04-22 17:23:49 -0400

    Setting up Mongoose (1) setting Mongoose connect to Mlab

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -1,9 +1,20 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+// connect to mongodb, UserName=jenhao; Password=jenhao
+const db = "mongodb://jenhao:jenhao@ds147659.mlab.com:47659/turco_persian_rug"
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function(err){
+    if(err){
+        console.error("Error!" + err);
+    }
+});
+
 
 router.get('/',function(req,res){
-    res.send('api works222');
+    res.send('api works1');
 });
 
 module.exports = router;
