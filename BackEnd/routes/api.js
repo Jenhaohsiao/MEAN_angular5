7b9d49262a4fa3ee996a88800de6e571606906c3
JenHao <jenhao.hsiao@gmail.com>
2018-04-22 17:42:58 -0400

    Added fetch DB data function 'api.js' added function to get All and Single data from DB

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Rug = require('../models/rug');
 
 // connect to mongodb, UserName=jenhao; Password=jenhao
 const db = "mongodb://jenhao:jenhao@ds147659.mlab.com:47659/turco_persian_rug"
@@ -12,9 +13,32 @@ mongoose.connect(db, function(err){
     }
 });
 
-
-router.get('/',function(req,res){
-    res.send('api works1');
+// fetch all rugs data from DB
+router.get('/rugs',function(req,res){
+    console.log('Get request for all rugs');
+    Rug.find({})
+    .exec(function(err, rugs){
+        if (err){
+            console.log("Error retrieving rugs");
+        }else {
+            res.json(rugs);
+        }
+    });
 });
+
+// fetch single rug data from DB
+router.get('/rugs/:id',function(req,res){
+    console.log('Get request for single rug');
+    Rug.findById(req.params.id)
+    .exec(function(err, rug){
+        if (err){
+            console.log("Error retrieving rug");
+        }else {
+            res.json(rug);
+        }
+    });
+});
+
+
 
 module.exports = router;
