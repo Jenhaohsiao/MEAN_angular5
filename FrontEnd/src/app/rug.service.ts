b4091dcf3aed5ab218018268a26b59e2cb086483
JenHao <jenhao.hsiao@gmail.com>
2018-04-23 16:15:20 -0400

    Added "update item into MongoDB" function (1) "rug.service.ts" added putUrl and "updateRug" method to update data to DB (2) "rug-center" added listener and method to pass the data to "rugService" (3) "rug-detail" added trigger and method to pass the rug data to "rugDetail"

--- FrontEnd/src/app/rug.service.ts
+++ FrontEnd/src/app/rug.service.ts
@@ -8,11 +8,11 @@ export class RugService {
 
   // get url by the path we set on "service.js" and "api.js"
   private _getUrl = '/api/rugs';
-  private _postUrl = 'api/rug';
+  private _postUrl = '/api/rug';
+  private _putUrl = '/api/rug/';
 
   // dependency injection to get and instance of Http
   constructor(private _http: Http) { }
-
 
   getRugsFromHttp() {
     // Get response mapped to JSON
@@ -24,8 +24,17 @@ export class RugService {
     const headers = new Headers({'Content-Type': 'application/json'});
     const options = new RequestOptions({ headers: headers});
     // Mapped response to JSON and pass JSON stringify file to DataBase
-    return this._http.post(this._postUrl, JSON.stringify(rug), options).map((response: Response) => response.json());
+    return this._http.post(this._postUrl, JSON.stringify(rug), options)
+    .map((response: Response) => response.json());
   }
+
+  updateRug(rug: Rug) {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({ headers: headers});
+    return this._http.put(this._putUrl + rug._id, JSON.stringify(rug), options)
+    .map((response: Response) => response.json());
+  }
+
 
 }
 
